376604e7db74dcfd274af653ab345bd5799ad6e3
grnimrod <nimrod.grandpierre@gmail.com>
2024-10-21 17:55:37 +0200

    Added circles for data points, applied year filter

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,20 @@
-import { create, geoPath, geoBounds, selectAll } from "d3";
+import {
+  create,
+  geoAlbersUsa,
+  geoPath,
+  geoBounds,
+  selectAll,
+  timeParse,
+} from "d3";
 import * as topojson from "topojson-client";
 
-const usAtlasUrl = "https://unpkg.com/us-atlas@3.0.1/counties-albers-10m.json"; // data is already Albers projected, no need to create separate projection
+const usAtlasUrl = "https://unpkg.com/us-atlas@3.0.1/counties-10m.json";
 const firesJson = "../fires.json";
 
 Promise.all([
   fetch(usAtlasUrl).then((response) => response.json()),
   fetch(firesJson).then((response) => response.json()),
 ]).then(([topoJsonData, firesData]) => {
-  console.log(firesData);
   const width = window.innerWidth;
   const height = window.innerHeight;
 
@@ -17,7 +23,11 @@ Promise.all([
     .attr("height", height)
     .attr("style", "max-width: 100%; height: auto;");
 
-  const path = geoPath();
+  const projection = geoAlbersUsa()
+    .translate([width / 2, height / 2])
+    .scale(800);
+
+  const path = geoPath().projection(projection);
 
   // Calculate bounding box of the map data
   const bounds = geoBounds(
@@ -57,6 +67,38 @@ Promise.all([
         )
       )
     );
+
+  // Filter data points where return of projection is null
+  const validProjectedFires = firesData.filter((d) => {
+    const projected = projection([d.LONGITUDE, d.LATITUDE]);
+    return projected !== null;
+  });
+
+  // Parse date fields of fires data
+  const parseDate = timeParse("%Y-%m-%d %H:%M:%S");
+
+  validProjectedFires.forEach((d) => {
+    d.DISCOVERY_DATE = parseDate(d.DISCOVERY_DATE);
+    d.CONT_DATE = parseDate(d.CONT_DATE);
+  });
+
+  // Filter data of specific year, e.g. 2005
+  const yearData = validProjectedFires.filter(
+    (d) => d.DISCOVERY_DATE.getFullYear() === 2005
+  );
+
+  console.log(yearData);
+
+  // Append fire data points as circles to SVG
+  svg
+    .selectAll("circle")
+    .data(yearData)
+    .join("circle")
+    .attr("cx", (d) => projection([d.LONGITUDE, d.LATITUDE])[0])
+    .attr("cy", (d) => projection([d.LONGITUDE, d.LATITUDE])[1])
+    .attr("r", 2)
+    .attr("fill", "blue")
+    .attr("opacity", 0.5);
 
   // Append SVG element to the body
   document.body.append(svg.node());
